fc19677ea3b9eb947c92e3b5dcf77671f1f4df5e
paopapaopao <paolo.casugay@gmail.com>
2021-11-03 15:02:02 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import Paypal from './components/Paypal';
+import logo from './images/logo.png';
+import product from './images/product-transparent.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="menu-toggle">
+        <i class="fas fa-bars"></i>
+      </div>
+      <div class="overlay"></div>
+
+      <nav>
+        <i class="fas fa-times"></i>
+        <div class="logo">
+          <img src={logo} alt="jumpman logo"></img>
+        </div>
+
+        <div class="nav-middle">
+          <ul>
+            <li><a href="#">Collection</a></li>
+            <li><a href="#">Catalog</a></li>
+            <li><a href="#">About Us</a></li>
+          </ul>
+        </div>
+
+        <div class="nav-right">
+          <ul>
+            <li>
+              <a href="#">
+                <i class="fas fa-search"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fas fa-user"></i> Account
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fas fa-cart"></i>Cart
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className="main">
+        <div className="tone-1">
+          <div className="product-info">
+            <h1>Jordan 1 Retro High Off-White University Blue</h1>
+            <p>Time for some Tobacco Road vibes with these Jordan 1 Retro Off-Whites.</p>
+            <h2>$1,625</h2>
+            {/* <div className="info-btns">
+              <a href="#" className="btn discover-btn">DISCOVER</a>
+              <a href="#" className="btn cart-btn">ADD TO CART</a>
+            </div> */}
+            <Paypal></Paypal>
+          </div>
+        </div>
+
+        <div className="tone-2">
+          <img src={product} alt="product"></img>
+        </div>
+      </div>
     </div>
   );
 }
